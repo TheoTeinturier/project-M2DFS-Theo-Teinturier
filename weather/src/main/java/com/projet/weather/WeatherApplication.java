9ea95c8e9179fb53e8ac9e4a2762c26e22b31065
Notsuko <teinturier.theo@gmail.com>
2021-11-19 11:30:18 +0100

    Récupération api des détails de la ville pour obtenir son code + affichage

--- weather/src/main/java/com/projet/weather/WeatherApplication.java
+++ weather/src/main/java/com/projet/weather/WeatherApplication.java
@@ -1,5 +1,6 @@
 package com.projet.weather;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -21,14 +22,27 @@ public class 	WeatherApplication {
 
 		@Autowired
 		RestTemplate restTemplate;
-
-		@RequestMapping(value = "/current/{key}", method = RequestMethod.GET)
-		public String getWeather(@PathVariable String key) {
+		//Permet de récupérer les informations météo d'une ville en fonction de son id
+		@ApiOperation(value = "Get details for weather", response = CurrentController.class, tags = "getMeteoIdVille")
+		@RequestMapping(value = "/meteoVille/{key}", method = RequestMethod.GET)
+		public String getMeteoIdVille(@PathVariable String key) {
 			String response = restTemplate.exchange("http://dataservice.accuweather.com/currentconditions/v1/{key}?apikey=tiRwrZIHTvt6t8A7AWQG6k3ugiDnHrTX&language=fr-fr",
 					HttpMethod.GET, null, new ParameterizedTypeReference<String>() {
 					}, key).getBody();
 			return response;
 		}
+
+		String nomVille;
+
+		//Permet de récupérer les détails d'une ville pour récupérer son code
+		@ApiOperation(value = "Recupération detailVille", response = CurrentController.class, tags = "getDetailVille")
+		@RequestMapping(value = "/detailVille/{nomVille}", method = RequestMethod.GET)
+		public String getCity(@PathVariable String nomVille) {
+			String response = restTemplate.exchange("http://dataservice.accuweather.com/locations/v1/cities/search?apikey=tiRwrZIHTvt6t8A7AWQG6k3ugiDnHrTX&q=" + nomVille + "&language=fr-fr",
+					HttpMethod.GET, null, new ParameterizedTypeReference<String>() {}, nomVille).getBody();
+			return response;
+		}
+
 
 		@Bean
 		public RestTemplate restTemplate() {
