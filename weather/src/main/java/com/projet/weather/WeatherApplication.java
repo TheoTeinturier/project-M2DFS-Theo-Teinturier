f158bbe7595331f07d0b4885fb586188164f64e1
Notsuko <teinturier.theo@gmail.com>
2021-11-19 10:52:18 +0100

    Première récupération et affichage du resultat sur la ville d'Ajaccio. Création d'un dossier screen permettant de visualiser les retours API

--- weather/src/main/java/com/projet/weather/WeatherApplication.java
+++ weather/src/main/java/com/projet/weather/WeatherApplication.java
@@ -1,13 +1,40 @@
 package com.projet.weather;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class WeatherApplication {
+public class 	WeatherApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherApplication.class, args);
-	}
 
-}
+	}
+	@RestController
+	public class CurrentController {
+
+		@Autowired
+		RestTemplate restTemplate;
+
+		@RequestMapping(value = "/current/{key}", method = RequestMethod.GET)
+		public String getWeather(@PathVariable String key) {
+			String response = restTemplate.exchange("http://dataservice.accuweather.com/currentconditions/v1/{key}?apikey=tiRwrZIHTvt6t8A7AWQG6k3ugiDnHrTX&language=fr-fr",
+					HttpMethod.GET, null, new ParameterizedTypeReference<String>() {
+					}, key).getBody();
+			return response;
+		}
+
+		@Bean
+		public RestTemplate restTemplate() {
+			return new RestTemplate();
+		}
+
+
+	}
+	}
